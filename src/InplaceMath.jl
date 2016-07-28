59e13ce99759c03d2eb0e672bcba61f5fba8adc2
juan <juanlopezm94@gmail.com>
2016-07-28 12:15:41 +0200

    Reorganize files and modify blas optimizations

--- src/InplaceMath.jl
+++ src/InplaceMath.jl
@@ -1,41 +1,7 @@
-import Base.LinAlg.BLAS: blascopy!, scal!, axpy!
-
-# Potential loss of precission when A and O have different types
-copy!(A::AbstractArray, O::AbstractArray) = blascopy!(length(A), A, 1, O, 1)
-
-# Add robustness to blascopy!
-function blascopy!(n::Number, DX::AbstractArray, incx::Integer, DY::AbstractArray, incy::Integer)
-    x = 1
-    y = 1
-    nx = min(n, length(DX))
-    ny = min(n, length(DY))
-    while x <= nx && y <= ny
-        DY[y] = DX[x]
-        x += incx
-        y += incy
-    end
-    DY
-end
-
-# Potential errors when cant convert typeof(a*O[n]) to typeof(O[n])
-scale!(a::Number, O::AbstractArray) = scal!(length(O), a, O, 1)
-
-# Add robustness to scal!
-function scal!(n::Integer, DA::Number, DX::AbstractArray, incx::Integer)
-    i = 1
-    n = min(n,length(DX))
-    while i <= n
-        DX[i] *= DA
-        i += incx
-    end
-    DX
-end
-
 ############
 # Addition #
 ############
 
-# Turns out to be more expensive than allocating memory when adding few things
 _add!(O::AbstractArray, ::Tuple{}) = O
 _add!(O::AbstractArray, As::Tuple) = _add!(axpy!(1, As[1], O), Base.tail(As))
 _add!{N}(::Type{Inplace{N}}, As...) = _add!(As[N], cat_tuple(As[1:N-1],As[N+1:end]))
@@ -45,7 +11,7 @@ _add!(O::AbstractArray, As...) = _add!(copy!(As[1], O), Base.tail(As))
 # Substraction #
 ################
 
-# Turns out to be more expensive than allocating memory
+# Turns out to be more expensive than not allocating memory
 function _sub!(O::AbstractArray, A::AbstractArray, B::AbstractArray)
     size(O) == size(A) == size(B) || throw(DimensionMismatch("dimensions must match"))
     for i in 1:length(O)
--- src/InplaceOps.jl
+++ src/InplaceOps.jl
@@ -1,13 +1,120 @@
 module InplaceOps
 
 isdefined(:__precompile__) && __precompile__(true)
-
 include("Common.jl")
-
-# In place math functions for all types
 include("InplaceMath.jl")
 
-# Inplace macros
-include("Ops.jl")
+export @in1!, @in2!, @into!
+
+inplace_sym(s::Symbol) = inplace_sym(Operator{s}())
+
+inplace_sym{S}(::Operator{S}) = error("invalid operator")
+
+inplace_sym(::Operator{:+}) = :add!
+inplace_sym(::Operator{:-}) = :sub!
+inplace_sym(::Operator{:*}) = :mul!
+inplace_sym(::Operator{:\}) = :ldiv!
+inplace_sym(::Operator{:/}) = :rdiv!
+
+inplace_sym(::Operator{:.+}) = :badd!
+inplace_sym(::Operator{:.-}) = :bsub!
+inplace_sym(::Operator{:.*}) = :bmul!
+inplace_sym(::Operator{:.\}) = :bldiv!
+inplace_sym(::Operator{:./}) = :brdiv!
+
+
+op_ctranspose{T}(x::AbstractArray{T}) = CTranspose(x)
+op_ctranspose{T<:Real}(x::AbstractArray{T}) = Transpose(x)
+
+op_transpose{T}(x::AbstractArray{T}) = Transpose(x)
+
+typealias AbstractVMF Union{AbstractVecOrMat,Factorization}
+
+#TODO: Most of the 2-argument A_foo_B methods overwrite B, though there are some exceptions (e.g. QRPackedQ)
+mul!(::Type{Inplace{2}}, A::AbstractVMF, B::AbstractVMF) = A_mul_B!(A,B)
+mul!(::Type{Inplace{2}}, A::Transpose  , B::AbstractVMF) = At_mul_B!(A.obj,B)
+mul!(::Type{Inplace{2}}, A::CTranspose , B::AbstractVMF) = Ac_mul_B!(A.obj,B)
+
+ldiv!(::Type{Inplace{2}}, A::AbstractVMF, B::AbstractVMF) = A_ldiv_B!(A,B)
+ldiv!(::Type{Inplace{2}}, A::Transpose  , B::AbstractVMF) = At_ldiv_B!(A.obj,B)
+ldiv!(::Type{Inplace{2}}, A::CTranspose , B::AbstractVMF) = Ac_ldiv_B!(A.obj,B)
+
+rdiv!(::Type{Inplace{2}}, A::AbstractVMF, B::AbstractVMF) = A_rdiv_B!(A,B)
+rdiv!(::Type{Inplace{2}}, A::Transpose  , B::AbstractVMF) = At_rdiv_B!(A.obj,B)
+rdiv!(::Type{Inplace{2}}, A::CTranspose , B::AbstractVMF) = Ac_rdiv_B!(A.obj,B)
+
+mul!(O::AbstractVMF, A::AbstractVMF, B::AbstractVMF) = A_mul_B!(O,A,B)
+mul!(O::AbstractVMF, A::Transpose  , B::AbstractVMF) = At_mul_B!(O,A.obj,B)
+mul!(O::AbstractVMF, A::CTranspose , B::AbstractVMF) = Ac_mul_B!(O,A.obj,B)
+mul!(O::AbstractVMF, A::AbstractVMF, B::Transpose  ) = A_mul_Bt!(O,A,B.obj)
+mul!(O::AbstractVMF, A::Transpose  , B::Transpose  ) = At_mul_Bt!(O,A.obj,B.obj)
+mul!(O::AbstractVMF, A::CTranspose , B::Transpose  ) = Ac_mul_Bt!(O,A.obj,B.obj)
+mul!(O::AbstractVMF, A::AbstractVMF, B::CTranspose ) = A_mul_Bc!(O,A,B.obj)
+mul!(O::AbstractVMF, A::Transpose  , B::CTranspose ) = At_mul_Bc!(O,A.obj,B.obj)
+mul!(O::AbstractVMF, A::CTranspose , B::CTranspose ) = Ac_mul_Bc!(O,A.obj,B.obj)
+
+ldiv!(O::AbstractVMF, A::AbstractVMF, B::AbstractVMF) = A_ldiv_B!(O,A,B)
+ldiv!(O::AbstractVMF, A::Transpose  , B::AbstractVMF) = At_ldiv_B!(O,A.obj,B)
+ldiv!(O::AbstractVMF, A::CTranspose , B::AbstractVMF) = Ac_ldiv_B!(O,A.obj,B)
+ldiv!(O::AbstractVMF, A::AbstractVMF, B::Transpose  ) = A_ldiv_Bt!(O,A,B.obj)
+ldiv!(O::AbstractVMF, A::Transpose  , B::Transpose  ) = At_ldiv_Bt!(O,A.obj,B.obj)
+ldiv!(O::AbstractVMF, A::CTranspose , B::Transpose  ) = Ac_ldiv_Bt!(O,A.obj,B.obj)
+ldiv!(O::AbstractVMF, A::AbstractVMF, B::CTranspose ) = A_ldiv_Bc!(O,A,B.obj)
+ldiv!(O::AbstractVMF, A::Transpose  , B::CTranspose ) = At_ldiv_Bc!(O,A.obj,B.obj)
+ldiv!(O::AbstractVMF, A::CTranspose , B::CTranspose ) = Ac_ldiv_Bc!(O,A.obj,B.obj)
+
+rdiv!(O::AbstractVMF, A::AbstractVMF, B::AbstractVMF) = A_rdiv_B!(O,A,B)
+rdiv!(O::AbstractVMF, A::Transpose  , B::AbstractVMF) = At_rdiv_B!(O,A.obj,B)
+rdiv!(O::AbstractVMF, A::CTranspose , B::AbstractVMF) = Ac_rdiv_B!(O,A.obj,B)
+rdiv!(O::AbstractVMF, A::AbstractVMF, B::Transpose  ) = A_rdiv_Bt!(O,A,B.obj)
+rdiv!(O::AbstractVMF, A::Transpose  , B::Transpose  ) = At_rdiv_Bt!(O,A.obj,B.obj)
+rdiv!(O::AbstractVMF, A::CTranspose , B::Transpose  ) = Ac_rdiv_Bt!(O,A.obj,B.obj)
+rdiv!(O::AbstractVMF, A::AbstractVMF, B::CTranspose ) = A_rdiv_Bc!(O,A,B.obj)
+rdiv!(O::AbstractVMF, A::Transpose  , B::CTranspose ) = At_rdiv_Bc!(O,A.obj,B.obj)
+rdiv!(O::AbstractVMF, A::CTranspose , B::CTranspose ) = Ac_rdiv_Bc!(O,A.obj,B.obj)
+
+badd!{N}(::Type{Inplace{N}}, As...) = broadcast!(+,As[N],As...)
+bsub!{N}(::Type{Inplace{N}}, As...) = broadcast!(-,As[N],As...)
+bmul!{N}(::Type{Inplace{N}}, As...) = broadcast!(*,As[N],As...)
+bldiv!{N}(::Type{Inplace{N}}, As...) = broadcast!(\,As[N],As...)
+brdiv!{N}(::Type{Inplace{N}}, As...) = broadcast!(/,As[N],As...)
+add!{N}(t::Type{Inplace{N}}, As...) = _add!(t, As...)
+sub!{N}(t::Type{Inplace{N}}, As...) = _sub!(t, As...)
+
+badd!(O::AbstractArray, As...) = broadcast!(+,O,As...)
+bsub!(O::AbstractArray, As...) = broadcast!(-,O,As...)
+bmul!(O::AbstractArray, As...) = broadcast!(*,O,As...)
+bldiv!(O::AbstractArray, As...) = broadcast!(\,O,As...)
+brdiv!(O::AbstractArray, As...) = broadcast!(/,O,As...)
+add!(O::AbstractArray, As...) = _add!(O, As...)
+sub!(O::AbstractArray, As...) = _sub!(O, As...)
+
+replace_t(ex) = esc(ex)
+function replace_t(ex::Expr)
+    if ex.head == symbol("'")
+        :(op_ctranspose($(esc(ex.args[1]))))
+    elseif ex.head == symbol(".'")
+        :(op_transpose($(esc(ex.args[1]))))
+    else
+        esc(ex)
+    end
+end
+
+
+macro in1!(ex)
+    ex.head == :call || error("incorrect macro usage")
+    Expr(:call,inplace_sym(ex.args[1]),:(Inplace{1}),[replace_t(a) for a in ex.args[2:end]]...)
+end
+
+macro in2!(ex)
+    ex.head == :call || error("incorrect macro usage")
+    Expr(:call,inplace_sym(ex.args[1]),:(Inplace{2}),[replace_t(a) for a in ex.args[2:end]]...)
+end
+
+macro into!(ex)
+    ex.head == :(=) || error("incorrect macro usage")
+    out, ex = ex.args
+    Expr(:call,inplace_sym(ex.args[1]),esc(out),[replace_t(a) for a in ex.args[2:end]]...)
+end
 
 end
